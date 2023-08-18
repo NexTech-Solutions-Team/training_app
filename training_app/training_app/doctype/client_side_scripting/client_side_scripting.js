e00c52cc94a227c599aeaa4d3b96ad7c22ef9ae2
MohamedAbdulsalam96 <moha2016it@gmail.com>
2023-08-18 23:48:40 +0530

    Client Scripting

---
+++ training_app/training_app/doctype/client_side_scripting/client_side_scripting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Mohamed Abdulsalam and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Client Side Scripting', {
+	// refresh: function(frm) {
+
+	// }
+});
